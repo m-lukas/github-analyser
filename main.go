e7470c5859d5814db8c1550a2e0dc225e2abcd94
m-lukas <mail@lukasmueller.de>
2019-04-05 16:26:42 +0200

    Test http request

--- main.go
+++ main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	url := "https://api.github.com/repos/m-lukas/ibm_cloudfoundry/contents"
+
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		log.Fatal("API Request Creation failed!")
+		return
+	}
+
+	client := &http.Client{}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatal("API call failed")
+		return
+	}
+
+	defer resp.Body.Close()
+	log.Println(resp.Body)
+
 }
