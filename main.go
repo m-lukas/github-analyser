05c406e1828fcebd4060760b5d738bce1405e86f
m-lukas <mail@lukasmueller.de>
2019-04-07 20:52:12 +0200

    Add database session and proper initialization

--- main.go
+++ main.go
@@ -63,16 +63,4 @@ func main() {
 
 	log.Println("Server was stopped!")
 	os.Exit(0)
-	/*
-		collection := client.Database("test").Collection("test")
-
-		ctx, _ := context.WithTimeout(context.Background(), 5*time.Second)
-		res, err := collection.InsertOne(ctx, bson.M{"name": "pi", "value": 3.14159})
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		id := res.InsertedID
-		fmt.Println(id)
-	*/
 }
