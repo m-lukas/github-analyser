28247350ba438943b6b9cd6e2f79c7c49d247a2c
m-lukas <mail@lukasmueller.de>
2019-05-21 23:11:23 +0200

    Comment httputil package

--- httputil/reponse.go
+++ httputil/reponse.go
@@ -12,20 +12,24 @@ const (
 	INVALID_ARGUMENTS = 400
 )
 
+//ErrorResponse is used for marshaling http errors
 type ErrorResponse struct {
 	StatusCode int    `json:"-"`
 	Message    string `json:"msg"`
 }
 
+//NewError creates new ErrorResponse object with statuscode and message
 func NewError(statusCode int, message string) *ErrorResponse {
 	return &ErrorResponse{StatusCode: statusCode, Message: message}
 }
 
+//WriteError writes an http response using the ErrorResponse object
 func (e *ErrorResponse) WriteError(w http.ResponseWriter) {
 
 	w.Header().Set("Content-Type", "application/json")
 	w.Header().Set("Cache-Control", "no-cache, no-store, must-revalidate")
 
+	//check if statuscode is out of range
 	if e.StatusCode > 0 {
 		w.WriteHeader(e.StatusCode)
 	} else {
@@ -41,11 +45,13 @@ func (e *ErrorResponse) WriteError(w http.ResponseWriter) {
 	w.Write(bytes)
 }
 
+//WriteSuccess writes and http-response with a given statuscode and a marshaled data body
 func WriteSuccess(w http.ResponseWriter, statusCode int, data interface{}) {
 
 	w.Header().Set("Content-Type", "application/json")
 	w.Header().Set("Cache-Control", "no-cache, no-store, must-revalidate")
 
+	//check if statuscode is out of range
 	if statusCode > 0 {
 		w.WriteHeader(statusCode)
 	} else {
